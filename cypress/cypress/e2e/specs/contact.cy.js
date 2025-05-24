3d1887b2e1ed76f4874415ff132ce919c31188a3
Jonnas Christian <jonnaschristian@gmail.com>
2025-05-24 15:37:53 -0300

    Criado 08 testes automatizados web pro Cypress

---
+++ cypress/cypress/e2e/specs/contact.cy.js
@@ -0,0 +1,52 @@
+import { faker } from '@faker-js/faker';
+
+describe('Menu Contato', () => {
+  let contact;
+
+  before(() => {
+    cy.fixture('elements/contact.json').then((dados) => {
+      contact = dados;
+    });
+  });
+
+  it('Enviar mensagem com todos os campos válidos com sucesso', () => {
+    const nome = faker.person.fullName();
+    const email = faker.internet.email();
+    const telefone = faker.phone.number('(##) #####-####');
+    const mensagem = faker.lorem.sentence();
+
+    cy.get(contact.menu).click();
+    cy.get(contact.tituloContato).should('contain', 'Contact Me');
+
+    cy.get(contact.inputNome).type(nome);
+    cy.get(contact.inputEmail).type(email);
+    cy.get(contact.inputPhone).type(telefone);
+    cy.get(contact.inputMsg).type(mensagem);
+    cy.get(contact.btnEnviar).click();
+
+    cy.get(contact.mensagemSucesso).should('be.visible').and('contain', 'Form submission successful!');
+  });
+
+  it('Enviar mensagem sem os campos preenchidos deve dar erro', () => {
+    cy.get(contact.menu).click();
+    cy.get(contact.tituloContato).should('contain', 'Contact Me');
+
+    cy.get(contact.btnEnviar).should('have.class', 'disabled');
+  });
+
+  it('Não deve permitir enviar mensagem se o e-mail for inválido', () => {
+    const nome = faker.person.fullName();
+    const telefone = faker.phone.number('(##) #####-####');
+    const mensagem = faker.lorem.sentence();
+
+    cy.get(contact.menu).click();
+    cy.get(contact.tituloContato).should('contain', 'Contact Me');
+
+    cy.get(contact.inputNome).type(nome);
+    cy.get(contact.inputEmail).type('emailinvalido');
+    cy.get(contact.inputPhone).type(telefone);
+    cy.get(contact.inputMsg).type(mensagem);
+
+    cy.get(contact.mensagemErroEmail).should('be.visible').and('contain', 'Email is not valid.');
+  });
+});
